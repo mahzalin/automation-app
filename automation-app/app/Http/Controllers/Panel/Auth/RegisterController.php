fbc20a8645efa76db42a1f7727a2a28668fcfd5b
n.mahzali70@gmail.com <n.mahzali70@gmail.com>
2022-11-24 11:11:07 +0330

    change web.php

---
+++ automation-app/app/Http/Controllers/Panel/Auth/RegisterController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Panel\Auth;
+
+use App\Http\Controllers\Controller;
+
+class RegisterController extends Controller
+{
+    /**
+     * list of assets from wallet project
+     * @return bool
+     */
+    public function register()
+    {
+        return true;
+    }
+}
